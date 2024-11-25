1b1afa2273efcad8d06792c4e7dcbfe52df574d6
bclay <bclaypoole@gmail.com>
2024-11-25 10:52:28 -0800

    [NS1] Quake JSON decoding and error

---
+++ Earthquakes/Models/QuakeError.swift
@@ -0,0 +1,22 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+*/
+
+import Foundation
+
+enum QuakeError: Error {
+    case missingData
+}
+
+extension QuakeError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .missingData:
+            return NSLocalizedString(
+                "Found and will discard a quake missing a valid code, magnitude, place, or time.",
+                comment: ""
+            )
+        }
+    }
+}
