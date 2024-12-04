7d4d6f9e38d33afc09b12b0e8dcd6e01f163680c
bclay <bclaypoole@gmail.com>
2024-12-03 19:07:14 -0800

    [NS1] Add first unit test

---
+++ EarthquakesTests/EarthquakesTests.swift
@@ -0,0 +1,23 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+*/
+
+import XCTest
+@testable import Earthquakes
+
+class EarthquakesTests: XCTestCase {
+
+    func testGeoJSONDecoderDecodesQuake() throws {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        let quake = try decoder.decode(Quake.self, from:testFeature_nc73649170)
+        
+        XCTAssertEqual(quake.code, "73649170")
+        
+        let expectedSeconds = TimeInterval(1636129710550) / 1000
+        let decodedSeconds = quake.time.timeIntervalSince1970
+        
+        XCTAssertEqual(expectedSeconds, decodedSeconds, accuracy: 0.00001)
+    }
+}
