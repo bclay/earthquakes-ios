15fe5e74b0fb9f5865067446d9af8fe58b4bd3cb
bclay <bclaypoole@gmail.com>
2024-12-06 20:30:31 -0800

    [NS1] Set up client and provider

---
+++ Earthquakes/Models/QuakesProvider.swift
@@ -0,0 +1,26 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+*/
+
+import Foundation
+
+class QuakesProvider: ObservableObject {
+    
+    @Published var quakes: [Quake] = []
+    
+    let client: QuakeClient
+    
+    func fetchQuakes() async throws {
+        let latestQuakes = try await client.quakes
+        self.quakes = latestQuakes
+    }
+    
+    func deleteQuakes(atOffsets offsets: IndexSet) {
+        quakes.remove(atOffsets: offsets)
+    }
+    
+    init(client: QuakeClient = QuakeClient()) {
+        self.client = client
+    }
+}
